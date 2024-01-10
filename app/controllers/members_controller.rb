9c7b5c99e6b208094df46468416a0e30b07ecd59
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-01-10 19:01:27 +0200

    add member list page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,10 @@
 class MembersController < ApplicationController
+  before_action :set_current_tenant, only: [:index]
+  def index
+    @members = @current_tenant.members
+  end
+
+  def set_current_tenant
+    @current_tenant = Tenant.find(params[:tenant_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :members
-  resources :tenants
+  resources :tenants do
+    resources :members
+  end
   devise_for :users
   root "pages#home"
   get "dashboard", to: 'pages#dashboard'
